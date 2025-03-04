fc62110a9eb3fa413e8f1d66308bc91ebf454ae6
sdnytmt <ooaa0157@gmail.con>
2025-03-04 17:41:22 +0400

    init

---
+++ my_site/my_site/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import BigAutoField, DateTimeField
+from django.forms import CharField, DecimalField
+
+
+
+class MyUser(User):
+    number = models.CharField(length = 11)
+    birthday = models.DateTimeField
+    sex = models.BooleanField
+
+class Address(models.Model):
+    city = CharField(max_length=128)
+    street = CharField(max_length=128)
+    house = CharField(max_length=4)
+    user_id = models.ForeignKey(
+        MyUser, on_delete=models.DO_NOTHING,
+        null=True
+    )
+
+class Cards(models.Model):
+    number = CharField(length=16)
+    expiry_date = DateTimeField
+    user_id = models.ForeignKey(
+        MyUser, on_delete=models.DO_NOTHING,
+        null=True
+    )
+
+class Brand(models.Model):
+    name = CharField(max_length=32)
+    content = CharField(max_length = 10000)
+    registration_certificate = CharField(length=15)
+
+class BrandLogo(models.Model):
+    logo = CharField(max_length=256)
+    brand_id = models.ForeignKey(
+        Brand, on_delete=models.DO_NOTHING,
+        null=True
+    )
+
+class Product(models.Model):
+    name = CharField(max_length=32)
+    price = DecimalField(max_digits = 11, decimal_places = 3)
+    content = CharField(max_length = 10000)
+    quantity = BigAutoField()
+    brand_id = models.ForeignKey(
+        Brand, on_delete=models.DO_NOTHING,
+        null=True
+    )
+
+class ProductImage(models.Model):
+    img = CharField(max_length=256)
+    product_id = models.ForeignKey(
+        Product, on_delete=models.DO_NOTHING,
+        null=True
+    )
+
+
+
