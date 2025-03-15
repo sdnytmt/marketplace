8b844498b760906381bd3aef8f272e935fb8a6cb
sdnytmt <ooaa0157@gmail.con>
2025-03-15 18:27:26 +0400

    инициализация

--- my_site/my_site/admin.py
+++ my_site/my_site/admin.py
@@ -1,6 +1,16 @@
 from django.contrib.admin import register
+from django.contrib import admin
 
 from . import models
 
-register(models.Favourite)
-
+admin.site.register(models.MyUser)
+admin.site.register(models.Address)
+admin.site.register(models.Cards)
+admin.site.register(models.Brand)
+admin.site.register(models.BrandLogo)
+admin.site.register(models.Product)
+admin.site.register(models.ProductImage)
+admin.site.register(models.Review)
+admin.site.register(models.ReviewPic)
+admin.site.register(models.Basket)
+admin.site.register(models.Favourite)
